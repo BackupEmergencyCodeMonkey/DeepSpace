ade5f83f870b6162e6ac9be450e3adb566a34b7e
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-03-04 17:55:42 -0500

    PID Class: Constructor complete

--- src/main/java/frc/robot/PID.java
+++ src/main/java/frc/robot/PID.java
@@ -1,12 +1,23 @@
 package frc.robot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class PID {
-  private int P;
-  private int I;
-  private int D;
-  private int F;
+  private double P;
+  private double I;
+  private double D;
+  private double F;
   private int slot;
-  
-public PID() {
+  private WPI_TalonSRX motor;
 
-}
+  public PID(WPI_TalonSRX driver, double pVal, double iVal, double dVal, double fVal, int slot) {
+    P = pVal;
+    I = iVal;
+    D = dVal;
+    F = fVal;
+    this.slot = slot;
+    motor = driver;
+    motor.config_kP(slot, P);
+    motor.config_kI(slot, I);
+    motor.config_kD(slot, D);
+    motor.config_kF(slot, F);
+  }
 }
