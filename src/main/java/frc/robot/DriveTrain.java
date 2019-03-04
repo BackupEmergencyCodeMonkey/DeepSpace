3cdf2dddfe0b0b7c87f75f44907b443703231e91
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-03-04 17:00:28 -0500

    Initial Commit: This is a test

---
+++ src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,59 @@
+package frc.robot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+public class DriveTrain {
+  private WPI_TalonSRX FrontLeft;
+  private WPI_TalonSRX MiddleLeft;
+  private WPI_TalonSRX BackLeft;
+  private WPI_TalonSRX FrontRight;
+  private WPI_TalonSRX MiddleRight;
+  private WPI_TalonSRX BackRight;
+  SpeedControllerGroup leftSide;
+  SpeedControllerGroup rightSide;
+  public DriveTrain() {
+    FrontLeft = new WPI_TalonSRX(1);
+    MiddleLeft = new WPI_TalonSRX(2);
+    BackLeft = new WPI_TalonSRX(3);
+    FrontRight = new WPI_TalonSRX(4);
+    MiddleRight = new WPI_TalonSRX(5);
+    BackRight = new WPI_TalonSRX(6);
+    leftSide = new SpeedControllerGroup(FrontLeft, MiddleLeft, BackLeft);
+    rightSide = new SpeedControllerGroup(FrontRight, MiddleRight, BackRight);
+}
+  public DriveTrain(int FL, int ML, int BL, int FR, int MR, int BR) {
+    FrontLeft = new WPI_TalonSRX(FL);
+    MiddleLeft = new WPI_TalonSRX(ML);
+    BackLeft = new WPI_TalonSRX(BL);
+    FrontRight = new WPI_TalonSRX(FR);
+    MiddleRight = new WPI_TalonSRX(MR);
+    BackRight = new WPI_TalonSRX(BR);
+    leftSide = new SpeedControllerGroup(FrontLeft, MiddleLeft, BackLeft);
+    rightSide = new SpeedControllerGroup(FrontRight, MiddleRight, BackRight);
+}
+  public DriveTrain(WPI_TalonSRX FL, WPI_TalonSRX ML, WPI_TalonSRX BL, WPI_TalonSRX FR, WPI_TalonSRX MR, WPI_TalonSRX BR) {
+    FrontLeft = FL;
+    MiddleLeft = ML;
+    BackLeft = BL;
+    FrontRight = FR;
+    MiddleRight = MR;
+    BackRight = BR;
+    leftSide = new SpeedControllerGroup(FrontLeft, MiddleLeft, BackLeft);
+    rightSide = new SpeedControllerGroup(FrontRight, MiddleRight, BackRight);
+}
+  public DriveTrain(SpeedControllerGroup L, SpeedControllerGroup R) {
+    FrontLeft = new WPI_TalonSRX(1);
+    MiddleLeft = new WPI_TalonSRX(2);
+    BackLeft = new WPI_TalonSRX(3);
+    FrontRight = new WPI_TalonSRX(4);
+    MiddleRight = new WPI_TalonSRX(5);
+    BackRight = new WPI_TalonSRX(6);
+    leftSide = L;
+    rightSide = R;
+}
+  public void arcadeDrive() {
+    
+  }
+}
