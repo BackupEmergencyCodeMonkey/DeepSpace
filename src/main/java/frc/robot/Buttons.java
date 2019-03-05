7eb8365a029a213b437d867f6dad69050b4dd03e
BackupEmergencyCodeMonkey <wackjertz@gmail.com>
2019-03-04 20:39:46 -0500

    Buttons Class Removed

--- src/main/java/frc/robot/Buttons.java
+++
@@ -1,5 +0,0 @@
-package frc.robot;
-public class Buttons{
-    public Buttons(){}
-    public String leftStick [] = new String[] {"ballIn","", "elevatorDown", "elevatorUp", "elbowDown", "elbowUp"};
-}
